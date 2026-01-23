ab4baf9220c5de94e2eda99e2fa97a496e6ac830
cilxry <cilxry@outlook.com>
2026-01-23 14:28:37 +0800

    feat 重写了navbar的部分和小修改

--- src/content/introTexts.ts
+++ src/content/introTexts.ts
@@ -1,4 +1,3 @@
-// src/content/lists.ts
 export const items = [
   // {
   //   header: "这是一段标题",
@@ -10,21 +9,41 @@ export const items = [
     header_eng: "about me",
     content:
       "Hi There. 👋<br>\
-      我是 CILXRY（也叫小十柒、ZYX）。",
+      我是 CILXRY（也叫小十柒、ZYX）。\
+      目前是 <strong>一名 17 岁的中职校学生。</strong><br>",
   },
   {
-    header: "先写点东西放在这",
-    header_eng: "Just Write Here to idk what it for.",
+    header: "会什么？",
+    header_eng: "what can i do?",
     content:
-      "目前是 <strong>一名 17 岁的中职校学生。</strong><br>\
-      会什么？<br>\
-      啥也不会，也就略懂一点编程和服务器知识……好吧，其实懂得不多。<br>\
+      "啥也不会，也就略懂一点编程和服务器知识……好吧，其实懂得不多。<br>\
       但别说，<strong>会吃饭会睡觉，我就能打败 1% 的生物了</strong>（迫真）。",
   },
   {
     header: "为什么做了这个网站",
     header_eng: "So why?",
+    content: "小孩不懂事，做着玩的。<br>",
+  },
+  {
+    header: "技术栈",
+    header_eng: "Technology Stack",
     content:
-      "小孩不懂事，做着玩的。<br>",
+      '<div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">\
+    <span class="px-3 py-1 text-sm">HTML/CSS</span>\
+    <span class="px-3 py-1 text-sm">Astro</span>\
+    <span class="px-3 py-1 text-sm">UnoCSS</span>\
+    <span class="px-3 py-1 text-sm">TypeScript</span>\
+    <span class="px-3 py-1 text-sm">Vue</span>\
+  </div>',
+  },
+  {
+    header: "写点东西凑字数",
+    header_eng: "Write something to fill the space",
+    content: "我不知道写点什么",
+  },
+  {
+    header: "写点东西凑字数",
+    header_eng: "Write something to fill the space",
+    content: "这里同样是凑字数的，别看了",
   },
 ];
