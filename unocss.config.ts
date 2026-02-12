04833df67023da8de00213d31cee89253e4a61d0
cilxry <cilxry@outlook.com>
2026-02-12 10:51:48 +0800

    - 从npm换到pnpm - 把vscode的设置提交到git - 添加了一个字体 - 修改了css和action配置

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  theme: {
+    fontFamily: {
+      baseFont: '"AiDianFengYaHei", sans-serif',
+    }
+  }
 })
