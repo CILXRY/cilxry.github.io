8f7be2cc0db842b37bdd04807d19964aea924035
CILXRY <cilxry@outlook.com>
2026-04-06 12:30:24 +0800

    conf: 更新配置文件中的行尾设置并添加版本配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,6 @@ module.exports = {
   root: true,
   env: { node: true },
   rules: {
-    'no-unused-vars': ['error', { vars: 'all', args: 'none' }]
-  }
-}
+    "no-unused-vars": ["error", { vars: "all", args: "none" }],
+  },
+};
