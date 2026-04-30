a21e5a80a36023ed865b4691897f5fdb9e8a256e
CILXRY <cilxry@outlook.com>
2026-04-30 11:17:26 +0800

    conf: 将config整合，使用index.ts

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+// "c-config.ts" "CLIX.ts" "mainConfig.ts"
+// 谁看了谁不懵逼
+
+export { items } from "./aboutConfig.ts";
+
+export { PersonalConfig, SiteConfig } from "./mainConfig.ts";
+
+export { navLogo, navItems } from "./navbarConfig.ts";
+
+export { hyperlinks } from "./Hyperlinks-Items.ts";
