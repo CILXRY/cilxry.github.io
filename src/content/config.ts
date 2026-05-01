cb35e8e8b4a88f66e716217163d76e58f63de164
CILXRY <cilxry@outlook.com>
2026-05-01 13:43:43 +0800

    fix(content-config): 整理content文件夹

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection } from "astro:content";
-import { z } from "astro/zod";
 import { glob } from "astro/loaders";
+import { postsSchema } from "./schema.ts";
 
 const posts = defineCollection({
   loader: glob({
@@ -8,22 +8,7 @@ const posts = defineCollection({
     base: "./src/content/posts",
     generateId: ({ entry }) => entry.replace(/\.md$/, ""),
   }),
-  schema: z.object({
-    title: z.string().default("标题不见了呢"),
-    tags: z.array(z.string().min(1).trim()).optional().nullable(),
-    category: z.string().optional().nullable(),
-    author: z.string().optional().nullable(),
-    draft: z.boolean().optional(),
-    description: z.string().default("描述被吃掉了啦"),
-    descGenAuthor: z.string().optional().nullable(),
-    descGenTime: z.coerce.date().optional(),
-    wordCount: z.number().optional().nullable(),
-    readingTime: z.number().optional().nullable(),
-    creation: z.coerce.date().default(new Date(0)),
-    published: z.coerce.date().default(new Date(0)),
-    warning: z.string().optional().nullable(),
-    // original: z.string().optional(),
-  }),
+  schema: postsSchema,
 });
 
 // const memory = defineCollection({
