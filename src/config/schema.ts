80631263576bb8860a82f0686314b1a3c5486457
CILXRY <cilxry@outlook.com>
2026-05-05 21:52:44 +0800

    feat(blogs): 博客页面优化

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -2,10 +2,14 @@ import { z } from "astro/zod";
 
 export const postsSchema = z.object({
   title: z.string().default("标题不见了呢"),
-  tags: z.array(z.string().min(1).trim()).nullable(),
-  category: z.string().nullable(),
-  author: z.string().nullable(),
-  draft: z.boolean(),
+  tags: z.array(z.string().min(1).trim()).nullable().default(null),
+  category: z
+    .union([z.string(), z.array(z.string())])
+    .transform((val) => (Array.isArray(val) ? val.join(", ") : val))
+    .nullable()
+    .default(null),
+  author: z.string().nullable().default(null),
+  draft: z.boolean().default(true),
   description: z.string().default("描述被吃掉了啦"),
   descGenAuthor: z.string().optional().nullable(),
   descGenTime: z.coerce.date().optional(),
